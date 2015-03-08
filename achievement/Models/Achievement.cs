e50c4cc26b5fb28bd45f1e8d7783057813f9df22
SriRajeswari <shripriya996@gmail.com>
2015-03-08 12:14:14 +0530

    Added annotations

--- achievement/Models/Achievement.cs
+++ achievement/Models/Achievement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,16 @@ namespace achievement.Models
     public class Achievement
     {
         public int ID { get; set; }
+        [Required]
+        [StringLength(100)]
+        [RegularExpression("^([a-zA-Z0-9 .&'-]+)$", ErrorMessage = "Invalid Name")]
         public String Name { get; set; }
+        [Url]
+        [Display(Description = "Youtube URL")]
+        [RegularExpression("^(https://www.youtube.com/)", ErrorMessage = "Only Youtube Url")]
         public String URL { get; set; }
+        [Required]
+        [StringLength(10000)]
         public String Acheivement { get; set; }
         public DateTime Created { get; set; }
         public string Createdby { get; set; }
