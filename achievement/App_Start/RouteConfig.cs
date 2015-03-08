1158bdab7e3beebbf3dcdd39cfd09f78970e6ec1
SriRajeswari <shripriya996@gmail.com>
2015-03-08 18:00:48 +0530

    Chnaged default homepage

--- achievement/App_Start/RouteConfig.cs
+++ achievement/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace achievement
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Achievement", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
